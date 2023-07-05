fa205ac5741fa72c918c9f3c5f2d8122829fca21
Adrien Touzouli <contact@ad2ien.dev>
2023-07-05 18:48:54 +0200

    🎉 first commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Matrix bridge test bench
+
+> The goal of this repo is improve skills about Matrix and Matrix bridges
+
+With the deployment of a Synapse instance with it's element client and 2 bridges : slack and discord.
+
+## Docs
+
+## Prerequisites
+
+- a server with docker and docker-compose installed 
+- traefik running and configured with a domain
+
+## Installations
+
+Follow these:
+- [Matrix](matrix/readme.md)
+- [Slack-bridge]()
---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+env_file=".env"
+
+function replace_everywhere() {
+
+    files=$(find . -type f \( -name "*.yaml" -o -name "*.yml" -o -name ".env" \))
+
+    for file in $files; do
+        # Replace the old value with the new value using sed
+        sed -i "s/{{ $1 }}/$2/g" "$file"
+    done
+
+}
+
+echo Replace variables
+while IFS= read -r line || [[ -n "$line" ]]; do
+    # Skip empty lines and lines starting with #
+    if [[ -z "$line" || "$line" == "#"* ]]; then
+        continue
+    fi
+    
+    # Extract the key and value from the line
+    IFS="=" read -r key value <<< "$line"
+    
+    # Trim leading and trailing whitespace from the key and value
+    key="${key#"${key%%[![:space:]]*}"}"
+    key="${key%"${key##*[![:space:]]}"}"
+    value="${value#"${value%%[![:space:]]*}"}"
+    value="${value%"${value##*[![:space:]]}"}"
+    
+    # Perform operations with the key and value
+    echo "Replace: $key"
+    
+    replace_everywhere $key $value
+    
+done < "$env_file"
+
+echo set passwords...
+files=$(find . -type f \( -name "*.yaml" -o -name "*.yml" -o -name ".env" \))
+
+# Iterate over each file
+for file in $files; do
+    # Replace the old value with the new value using sed
+    sed -i "s/{{ password }}/$(openssl rand -base64 12)/g" "$file"
+done
+
+echo finished
