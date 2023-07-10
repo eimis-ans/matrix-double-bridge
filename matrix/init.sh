04a4c703a8c81003af34c062ab0cbd784f091da2
Adrien Touzouli <contact@ad2ien.dev>
2023-07-10 16:54:28 +0200

    🧱 domain variable consistence

--- matrix/init.sh
+++ matrix/init.sh
@@ -5,7 +5,7 @@ source .env
 docker run -it --rm \
     -v "matrix_matrix-data:/data" \
     -v "./mx-conf:/mx-conf" \
-    -e SYNAPSE_SERVER_NAME=$DOMAIN \
+    -e SYNAPSE_SERVER_NAME=matrix.$DOMAIN \
     -e SYNAPSE_CONFIG_PATH=/mx-conf/homeserver.yaml \
     -e SYNAPSE_REPORT_STATS=yes \
     matrixdotorg/synapse:v1.83.0 generate
