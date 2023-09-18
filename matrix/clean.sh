c5aa08d44656ec86cd2d859a8e5affed5ca1e172
Pipo Bimbo <heroinedor@gmail.com>
2023-09-18 17:27:36 +0200

    #140 discord bridge added to the docker-compose file and everything starts correctly

---
+++ matrix/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# delete all created files
+ rm -rf slack-bridge-config/config.yaml slack-bridge-config/slack-registration.yaml \
+    element-config.json mx-conf/ \
+    discord-bridge-config/discord-registration.yaml discord-bridge-config/config.yaml discord-bridge-config/discord.db* docker-compose.yml
